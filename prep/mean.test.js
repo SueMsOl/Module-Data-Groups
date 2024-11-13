c593db4243c75288a6568e763fa304c970e2463d
SueMsOl <sogandmsol17@gmail.com>
2024-11-13 00:18:58 +0000

    wrote and tested calculateMean()

--- prep/mean.test.js
+++ prep/mean.test.js
@@ -1,3 +1,15 @@
+const LIST = [7,3,50];
+function calculateMean(arg){
+    arg = LIST;
+    total = 0;
+    for (let item of arg){
+        total += item;
+    }
+    let meanCalc = total/arg.length
+    return meanCalc;
+}
+//console.log(calculateMean(LIST));
+
 test ("expected output is 20", ()=> {
     const LIST = [7,3,50];
     const CURRENT_OUTPUT = calculateMean(LIST);
