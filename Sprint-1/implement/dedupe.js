c613c89e63f1c4c814c07225e97af8ca6731d030
SueMsOl <sogandmsol17@gmail.com>
2024-11-15 16:15:52 +0000

    wrote and tested dedupe()

--- Sprint-1/implement/dedupe.js
+++ Sprint-1/implement/dedupe.js
@@ -3,6 +3,6 @@ function dedupe(list) {
     return [...new Set(list)];
   };
 
-module.exports = dedupe();
-const LIST = ['a','a','b',1,1];
-console.log(dedupe(LIST));
+module.exports = dedupe;
+//const LIST = ['a','a','b',1,1];
+//console.log(dedupe(LIST));
--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -1,7 +1,7 @@
-const ITEMS = [10,50,60,"a"];
+//const ITEMS = [10,50,60,"a"];
 function findMax(elements) {
     let numericItems = elements.filter(value => typeof value === 'number');
     return numericItems.length > 0 ? Math.max(...numericItems) : -Infinity;
 }
-console.log(findMax(ITEMS));
+//console.log(findMax(ITEMS));
 module.exports = findMax;
