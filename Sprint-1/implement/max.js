4f09cb719c2a92ccee34c874df0e827e9bd6e9b7
SueMsOl <sogandmsol17@gmail.com>
2024-11-15 00:57:26 +0000

    wrote and tested findMax()

--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -1,4 +1,7 @@
+//const ITEMS = ["a","b","c"];
 function findMax(elements) {
+    let numericItems = elements.filter(value => typeof value === 'number');
+    return numericItems.length > 0 ? Math.max(...numericItems) : -Infinity;
 }
-
+//console.log(findMax(ITEMS));
 module.exports = findMax;
