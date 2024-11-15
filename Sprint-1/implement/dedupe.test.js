690a6121aa0260809d32822216d9d77b168d1cd9
SueMsOl <sogandmsol17@gmail.com>
2024-11-15 00:25:34 +0000

    wrote dedupe() function but dedupe.test.js does not work (CYF solution file also fails and outputs error in terminal)

--- Sprint-1/implement/dedupe.test.js
+++ Sprint-1/implement/dedupe.test.js
@@ -8,7 +8,6 @@ In this kata, you will need to deduplicate the elements of an array
 
 E.g. dedupe(['a','a','a','b','b','c']) target output: ['a','b','c']
 E.g. dedupe([5, 1, 1, 2, 3, 2, 5, 8]) target output: [5, 1, 2, 3, 8]
-E.g. dedupe([1, 2, 1]) target output: [1, 2]
 */
 
 // Acceptance Criteria:
@@ -16,7 +15,6 @@ E.g. dedupe([1, 2, 1]) target output: [1, 2]
 // Given an empty array
 // When passed to the dedupe function
 // Then it should return an empty array
-test.todo("given an empty array, it returns an empty array");
 
 // Given an array with no duplicates
 // When passed to the dedupe function
@@ -24,4 +22,20 @@ test.todo("given an empty array, it returns an empty array");
 
 // Given an array with strings or numbers
 // When passed to the dedupe function
-// Then it should remove the duplicate values, preserving the first occurence of each element
+// Then it should remove the duplicate values
+
+describe("dedupe", () => {
+  it("returns the array without duplicates", () => {
+    expect(dedupe(['a','a','a','b','b','c'])).toStrictEqual(['a','b','c']);
+    expect(dedupe([5, 1, 1, 2, 3, 2, 5, 8])).toStrictEqual([5, 1, 2, 3, 8]);
+  });
+
+  it("returns the original array when the original array does not contain duplicates", () => {
+    expect(dedupe(['a','b','c'])).toStrictEqual(['a','b','c']);
+    expect(dedupe([5, 1, 2, 3, 8])).toStrictEqual([5, 1, 2, 3, 8]);
+  });
+
+  it("returns an array when an empty array is passed to the function", () => {
+    expect(dedupe([])).toStrictEqual([]);
+  });
+});
