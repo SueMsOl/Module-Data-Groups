72a6a403db16710a8def3fe1f17bcec9c1957949
SueMsOl <sogandmsol17@gmail.com>
2024-11-26 13:18:34 +0000

    wrote and tested tally function

--- Sprint-2/implement/tally.js
+++ Sprint-2/implement/tally.js
@@ -1,3 +1,22 @@
-function tally() {}
+let test = ['a', 'a', 'b', 'c'];
 
+function tally(arg) {
+    let frequency = 0;
+    let object = {};
+    if (arg.length === 0){
+        return object;
+    }
+    for(let i = 0; i < test.length; i++){
+        if(object[arg[i]]){
+            frequency++;
+            object[arg[1]]=frequency;
+        }
+        else{
+            frequency = 1;
+            object[arg[i]]=frequency;
+        }
+    }
+    return object;
+}
+console.log(tally(test));
 module.exports = tally;
